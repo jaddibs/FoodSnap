5d054cdc99fdafeef1dd5777b16ab3e3d9fdcb38
Jad Dibs <jaddibs@gmail.com>
2025-05-04 18:40:19 -0500

    Initial commit

---
+++ FoodSnap/FoodSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodSnapApp.swift
+//  FoodSnap
+//
+//  Created by Jad Dibs on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodSnapTests/FoodSnapTests.swift
@@ -0,0 +1,16 @@
+//
+//  FoodSnapTests.swift
+//  FoodSnapTests
+//
+//  Created by Jad Dibs on 5/4/25.
+//
+
+import Testing
+
+struct FoodSnapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
